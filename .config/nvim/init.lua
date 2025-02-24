8b762e6f89851b73c527193ae9d23c2af73cb694
itswicked <abhishekssm@outlook.com>
2025-02-24 19:14:10 +0530

    fixed the escape character typo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -86,7 +86,7 @@ vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
-vim.keymap.set('x', '<leader>p', '\'_dP')
+vim.keymap.set('x', '<leader>p', '"_dP')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
