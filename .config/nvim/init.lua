1daa868e64f06eea02c3ba2ef483a6de935f8b21
itswicked <abhishekssm@outlook.com>
2025-07-04 02:56:22 +0530

    alacritty config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -186,8 +186,8 @@ require('lazy').setup({
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {
-      indent = { char = "┊" }, -- Use thin dotted lines
-      scope = { enabled = true }, -- Highlight current scope
+      indent = { char = "│" }, -- Use thin dotted lines
+      scope = { enabled = false }, -- Highlight current scope
     },
   },
 
@@ -697,17 +697,19 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'blazkowolf/gruber-darker.nvim',
+    -- 'blazkowolf/gruber-darker.nvim',
     -- 'folke/tokyonight.nvim',
     -- 'oxfist/night-owl.nvim',
+    'EdenEast/nightfox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'gruber-darker'
+      -- vim.cmd.colorscheme 'gruber-darker'
       -- vim.cmd.colorscheme 'tokyonight-night'
       -- vim.cmd.colorscheme 'night-owl'
+      vim.cmd.colorscheme 'carbonfox'
 
       -- Make the background transparent
       vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
